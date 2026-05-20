ed3af2e33c6c1b0fb79f7ec29e1419818a62a797
roltik888 <dzgofs@gmail.com>
2026-05-20 09:18:06 +0200

    Global update added: cheat mode  win/lose and more

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            timer1 = new System.Windows.Forms.Timer(components);
+            panelmenu = new Panel();
+            NormBtn = new Button();
+            CheatBtn = new Button();
+            BtnMediums = new Button();
+            BtnHards = new Button();
+            BtnEasys = new Button();
+            btnStart = new Button();
+            btnEasy = new Button();
+            btnMedium = new Button();
+            btnHard = new Button();
+            panelmenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // panelmenu
+            // 
+            panelmenu.BackgroundImage = (Image)resources.GetObject("panelmenu.BackgroundImage");
+            panelmenu.Controls.Add(NormBtn);
+            panelmenu.Controls.Add(CheatBtn);
+            panelmenu.Controls.Add(BtnMediums);
+            panelmenu.Controls.Add(BtnHards);
+            panelmenu.Controls.Add(BtnEasys);
+            panelmenu.Dock = DockStyle.Fill;
+            panelmenu.Location = new Point(0, 0);
+            panelmenu.Name = "panelmenu";
+            panelmenu.Size = new Size(784, 561);
+            panelmenu.TabIndex = 0;
+            panelmenu.Paint += panelmenu_Paint;
+            // 
+            // NormBtn
+            // 
+            NormBtn.BackColor = Color.Transparent;
+            NormBtn.Cursor = Cursors.Hand;
+            NormBtn.FlatAppearance.BorderSize = 0;
+            NormBtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            NormBtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            NormBtn.FlatStyle = FlatStyle.Flat;
+            NormBtn.Location = new Point(412, 384);
+            NormBtn.Name = "NormBtn";
+            NormBtn.Size = new Size(175, 108);
+            NormBtn.TabIndex = 7;
+            NormBtn.UseVisualStyleBackColor = false;
+            NormBtn.Click += NormBtn_Click;
+            // 
+            // CheatBtn
+            // 
+            CheatBtn.BackColor = Color.Transparent;
+            CheatBtn.Cursor = Cursors.Hand;
+            CheatBtn.FlatAppearance.BorderSize = 0;
+            CheatBtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            CheatBtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            CheatBtn.FlatStyle = FlatStyle.Flat;
+            CheatBtn.Location = new Point(215, 384);
+            CheatBtn.Name = "CheatBtn";
+            CheatBtn.Size = new Size(175, 108);
+            CheatBtn.TabIndex = 6;
+            CheatBtn.UseVisualStyleBackColor = false;
+            CheatBtn.Click += CheatBtn_Click;
+            // 
+            // BtnMediums
+            // 
+            BtnMediums.BackColor = Color.Transparent;
+            BtnMediums.Cursor = Cursors.Hand;
+            BtnMediums.FlatAppearance.BorderSize = 0;
+            BtnMediums.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            BtnMediums.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            BtnMediums.FlatStyle = FlatStyle.Flat;
+            BtnMediums.Location = new Point(323, 238);
+            BtnMediums.Name = "BtnMediums";
+            BtnMediums.Size = new Size(161, 108);
+            BtnMediums.TabIndex = 5;
+            BtnMediums.UseVisualStyleBackColor = false;
+            BtnMediums.Click += BtnMediums_Click_1;
+            // 
+            // BtnHards
+            // 
+            BtnHards.BackColor = Color.Transparent;
+            BtnHards.Cursor = Cursors.Hand;
+            BtnHards.FlatAppearance.BorderSize = 0;
+            BtnHards.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            BtnHards.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            BtnHards.FlatStyle = FlatStyle.Flat;
+            BtnHards.Location = new Point(503, 238);
+            BtnHards.Name = "BtnHards";
+            BtnHards.Size = new Size(159, 108);
+            BtnHards.TabIndex = 3;
+            BtnHards.UseVisualStyleBackColor = false;
+            BtnHards.Click += BtnHards_Click;
+            // 
+            // BtnEasys
+            // 
+            BtnEasys.BackColor = Color.Transparent;
+            BtnEasys.Cursor = Cursors.Hand;
+            BtnEasys.FlatAppearance.BorderSize = 0;
+            BtnEasys.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            BtnEasys.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            BtnEasys.FlatStyle = FlatStyle.Flat;
+            BtnEasys.Location = new Point(147, 238);
+            BtnEasys.Name = "BtnEasys";
+            BtnEasys.Size = new Size(160, 108);
+            BtnEasys.TabIndex = 1;
+            BtnEasys.UseVisualStyleBackColor = false;
+            BtnEasys.Click += BtnEasys_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(0, 0);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 0;
+            // 
+            // btnEasy
+            // 
+            btnEasy.BackColor = Color.Transparent;
+            btnEasy.Cursor = Cursors.Hand;
+            btnEasy.FlatAppearance.BorderSize = 0;
+            btnEasy.FlatStyle = FlatStyle.Flat;
+            btnEasy.Location = new Point(133, 446);
+            btnEasy.Name = "btnEasy";
+            btnEasy.Size = new Size(191, 85);
+            btnEasy.TabIndex = 4;
+            btnEasy.UseVisualStyleBackColor = false;
+            // 
+            // btnMedium
+            // 
+            btnMedium.BackColor = Color.Transparent;
+            btnMedium.Cursor = Cursors.Hand;
+            btnMedium.FlatAppearance.BorderSize = 0;
+            btnMedium.FlatStyle = FlatStyle.Flat;
+            btnMedium.Location = new Point(345, 446);
+            btnMedium.Name = "btnMedium";
+            btnMedium.Size = new Size(213, 85);
+            btnMedium.TabIndex = 5;
+            btnMedium.UseVisualStyleBackColor = false;
+            // 
+            // btnHard
+            // 
+            btnHard.BackColor = Color.Transparent;
+            btnHard.Cursor = Cursors.Hand;
+            btnHard.FlatAppearance.BorderSize = 0;
+            btnHard.FlatStyle = FlatStyle.Flat;
+            btnHard.Location = new Point(571, 446);
+            btnHard.Name = "btnHard";
+            btnHard.Size = new Size(201, 85);
+            btnHard.TabIndex = 6;
+            btnHard.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Green;
+            ClientSize = new Size(784, 561);
+            Controls.Add(panelmenu);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyPress += Form1_KeyPress;
+            panelmenu.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Panel panelmenu;
+        private Button btnStart;
+        private Button btnHard;
+        private Button btnMedium;
+        private Button btnEasy;
+        private Button BtnHards;
+        private Button BtnEasys;
+        private Button BtnMediums;
+        private Button NormBtn;
+        private Button CheatBtn;
+    }
+}
---
+++ Snake/Form1.cs
@@ -0,0 +1,339 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Configuration;
+using System.Drawing.Drawing2D;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        Random xd = new Random();
+        List<Point> waz = new List<Point>();
+        Point jablko = new Point();
+        int score = 1;
+        int highscore = 1;
+        int speed = 20;
+        bool moveUP = false;
+        bool moveDOWN = false;
+        bool moveLEFT = false;
+        bool moveRIGHT = false;
+        bool lastMoveUP = false;
+        bool lastMoveDOWN = false;
+        bool lastMoveLEFT = false;
+        bool lastMoveRIGHT = false;
+        int rozmiarkwadrata = 20;
+        bool jablkozjedzone = false;
+        bool cheat = false;
+        bool normal = true;
+        int dlawygrania;
+        bool win = false;
+        bool pokazano = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lastMoveUP = moveUP;
+            lastMoveDOWN = moveDOWN;
+            lastMoveLEFT = moveLEFT;
+            lastMoveRIGHT = moveRIGHT;
+            bool czydodano = false;
+            if (score > highscore && czydodano == false)
+            {
+                czydodano = true;
+                highscore = score;
+                czydodano = false;
+            }
+            Point nowaglowa = waz[0];
+            if (moveDOWN == true)
+            {
+                nowaglowa.Y += speed;
+            }
+            if (moveUP == true)
+            {
+                nowaglowa.Y -= speed;
+            }
+            if (moveLEFT == true)
+            {
+                nowaglowa.X -= speed;
+            }
+            if (moveRIGHT == true)
+            {
+                nowaglowa.X += speed;
+            }
+            if (normal == true)
+            {
+                for (int i = 1; i < waz.Count; i++)
+                {
+                    if (nowaglowa.X == waz[i].X && nowaglowa.Y == waz[i].Y)
+                    {
+                        resetgame();
+                        return;
+                    }
+                }
+            }
+            waz.Insert(0, nowaglowa);
+            if (nowaglowa.X == jablko.X && nowaglowa.Y == jablko.Y && jablkozjedzone == false)
+            {
+                jablkozjedzone = true;
+                if (normal == true)
+                {
+                    score++;
+                }
+                int jablkoX = xd.Next(0, this.ClientSize.Width / rozmiarkwadrata) * rozmiarkwadrata;
+                int jablkoY = xd.Next(0, this.ClientSize.Height / rozmiarkwadrata) * rozmiarkwadrata;
+                jablko = new Point(jablkoX, jablkoY);
+                if (cheat == true)
+                {
+                    for (int i = 1; i < 6; i++)
+                    {
+                        waz.Add(waz[waz.Count - 1]);
+                    }
+                   
+                }
+                waz.Add(waz[waz.Count - 1]);
+                jablkozjedzone = false;
+            }
+            if (score == dlawygrania)
+            {
+
+                if (!pokazano)
+                {
+                    pokazano = true;
+                    resetgame1();
+                    MessageBox.Show("Wygrałeś");
+                }
+            }
+            else
+            {
+                waz.RemoveAt(waz.Count - 1);
+            }
+            if (normal == true)
+            {
+               
+                if (nowaglowa.X >= 761 || nowaglowa.X <= -1 || nowaglowa.Y >= 541 || nowaglowa.Y <= -1)
+                {
+                    resetgame();
+                }
+            }
+            if (cheat == true)
+            {
+                if (nowaglowa.X >= 761)
+                {
+                    moveRIGHT = false;
+                    moveLEFT = true;
+                    moveUP = false;
+                    moveDOWN = false;
+                }
+                if (nowaglowa.X <= -1)
+                {
+                    moveLEFT = false;
+                    moveRIGHT = true;
+                    moveUP = false;
+                    moveDOWN = false;
+                }
+                if (nowaglowa.Y >= 541)
+                {
+                    moveDOWN = false;
+                    moveUP = true;
+                    moveLEFT = false;
+                    moveRIGHT = false;
+                }
+                if (nowaglowa.Y <= -1)
+                {
+                    moveUP = false;
+                    moveDOWN = true;
+                    moveLEFT = false;
+                    moveRIGHT = false;
+                }
+            }
+            Invalidate();
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillRectangle(Brushes.Red, jablko.X, jablko.Y, rozmiarkwadrata, rozmiarkwadrata);
+            foreach (Point segment in waz)
+            {
+                g.FillRectangle(Brushes.Black, segment.X, segment.Y, rozmiarkwadrata, rozmiarkwadrata);
+            }
+            for (int x = 0; x < this.Width; x += rozmiarkwadrata)
+                g.DrawLine(Pens.DarkGreen, x, 0, x, this.Height);
+
+            for (int y = 0; y < this.Height; y += rozmiarkwadrata)
+                g.DrawLine(Pens.DarkGreen, 0, y, this.Width, y);
+            if (normal == true)
+            {
+                g.DrawString($"Score: {score}", new Font("Arial", 12), Brushes.White, 50, 10);
+                g.DrawString($"High score: {highscore}", new Font("Arial", 12), Brushes.White, 600, 10);
+            }
+            if (cheat == true)
+            {
+                g.DrawString($"Nie działa z cheat  modem", new Font("Arial", 12), Brushes.White, 50, 10);
+                
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            waz.Add(new Point(400, 300));
+            int maxrozmiarY = this.ClientSize.Height;
+            int maxrozmiarX = this.ClientSize.Width;
+            int jablkoX = xd.Next(0, this.ClientSize.Width / rozmiarkwadrata) * rozmiarkwadrata;
+            int jablkoY = xd.Next(0, this.ClientSize.Height / rozmiarkwadrata) * rozmiarkwadrata;
+            jablko = new Point(jablkoX, jablkoY);
+
+        }
+        private void resetgame()
+        {
+            timer1.Stop();
+            MessageBox.Show("Lose :( \t" +
+                $"Your score: {score} \t" +
+                $"Your Highest score: {highscore} \t");
+            score = 1;
+            moveUP = false;
+            moveDOWN = false;
+            moveLEFT = false;
+            moveRIGHT = false;
+            jablkozjedzone = false;
+            waz.Clear();
+            waz.Add(new Point(400, 300));
+            panelmenu.Visible = true;
+            int jablkoX = xd.Next(0, this.ClientSize.Width / rozmiarkwadrata) * rozmiarkwadrata;
+            int jablkoY = xd.Next(0, this.ClientSize.Height / rozmiarkwadrata) * rozmiarkwadrata;
+            jablko = new Point(jablkoX, jablkoY);
+        }
+        private void resetgame1()
+        {
+            timer1.Stop();
+            score = 1;
+            moveUP = false;
+            moveDOWN = false;
+            moveLEFT = false;
+            moveRIGHT = false;
+            jablkozjedzone = false;
+            waz.Clear();
+            waz.Add(new Point(400, 300));
+            panelmenu.Visible = true;
+            int jablkoX = xd.Next(0, this.ClientSize.Width / rozmiarkwadrata) * rozmiarkwadrata;
+            int jablkoY = xd.Next(0, this.ClientSize.Height / rozmiarkwadrata) * rozmiarkwadrata;
+            jablko = new Point(jablkoX, jablkoY);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void panelmenu_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        private void BtnEasys_Click(object sender, EventArgs e)
+        {
+            panelmenu.Visible = false;
+            this.Focus();
+            dlawygrania = 50;
+            timer1.Interval = 200;
+            timer1.Start();
+        }
+
+
+        private void BtnHards_Click(object sender, EventArgs e)
+        {
+            panelmenu.Visible = false;
+            this.Focus();
+            dlawygrania = 300;
+            timer1.Interval = 50;
+            timer1.Start();
+        }
+
+        private void BtnMediums_Click_1(object sender, EventArgs e)
+        {
+            panelmenu.Visible = false;
+            this.Focus();
+            dlawygrania = 150;
+            timer1.Interval = 100;
+            timer1.Start();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                if (!lastMoveDOWN)
+                {
+                    if (moveDOWN == false)
+                    {
+                        moveUP = true;
+                        moveDOWN = false;
+                        moveLEFT = false;
+                        moveRIGHT = false;
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                if (!lastMoveUP)
+                {
+                    if (moveUP == false)
+                    {
+                        moveUP = false;
+                        moveDOWN = true;
+                        moveLEFT = false;
+                        moveRIGHT = false;
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                if (!lastMoveRIGHT)
+                {
+                    if (moveRIGHT == false)
+                    {
+                        moveUP = false;
+                        moveDOWN = false;
+                        moveLEFT = true;
+                        moveRIGHT = false;
+                    }
+                }
+            }
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                if (!lastMoveLEFT)
+                {
+                    if (moveLEFT == false)
+                    {
+                        moveUP = false;
+                        moveDOWN = false;
+                        moveLEFT = false;
+                        moveRIGHT = true;
+                    }
+                }
+            }
+        }
+
+        private void CheatBtn_Click(object sender, EventArgs e)
+        {
+            cheat = true;
+            normal = false;
+            MessageBox.Show("Cheat mode włączony");
+        }
+
+        private void NormBtn_Click(object sender, EventArgs e)
+        {
+            normal = true;
+            cheat = false;
+            MessageBox.Show("Cheat mode wyłączony");
+        }
+    }
+}
